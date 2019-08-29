8e6245e89f9b31826be84d94c63db03ba3252cc2
Elena <lenaskr88@yandex.ru>
2019-08-29 21:00:33 +0300

    созданы модули для отрисовки списков trip-days и trip-events__list

---
+++ src/components/tripEventsList.js
@@ -0,0 +1,8 @@
+export const createTripEventsList = () => `
+<ul class="trip-events__list">
+  <li class="trip-events__item"></li>
+  <li class="trip-events__item"></li>
+  <li class="trip-events__item"></li>
+  <li class="trip-events__item"></li>
+</ul>
+`;
